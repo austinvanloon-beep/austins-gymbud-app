db9c88b49ae9cb00543b6ddd3d3b41f3bc684a7a
Austin Vanloon <wic404learn@gmail.com>
2025-07-31 08:04:46 -0600

    Initial commit setup boilerplates files etc

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const methodOverride = require('method-override')
+const dotenv = require('dotenv')
+const path = require('path')
+dotenv.config()
+const app = express()
+
+
+
+
+app.use(express)
